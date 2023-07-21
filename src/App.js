c16da7ff832d56ff4a7fbe7e9dde0d355ba4e6f0
priyam-11 <priyamd2001@gmail.com>
2023-07-21 15:56:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import Analytics from "./component/Analytics";
+import Newsletter from "./component/Newsletter";
+import Cards from "./component/Cards";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <Hero/>
+      <Analytics/>
+      <Newsletter/>
+      <Cards/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
